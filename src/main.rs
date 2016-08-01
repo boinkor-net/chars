a358b49b8402e4a6bfc456ccc1e5261fbbf9d920
Andreas Fuchs <asf@boinkor.net>
2016-08-01 00:34:00 -0700

    Read and interpret numerics (dec, hex, oct, bin)
    Chars now can read numbers from the arg list and interpret them in a
    variety of bases.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ extern crate unicode_names;
 
 use std::env;
 use std::fmt;
+use std::char;
 
 struct Describable {
     c: char,
@@ -10,7 +11,13 @@ struct Describable {
 impl fmt::Display for Describable {
     fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
         match unicode_names::name(self.c) {
-            Some(n) =>  write!(f, "{} = {}", self.c, n),
+            // TODO: restructure this for high-bit chars
+            Some(n) => {
+                let num = self.c as u32;
+                let quote : String = self.c.escape_default().collect();
+                write!(f, "{} = {}, {:}, 0x{:x}, 0{:o}, bits {:b}: prints as {}",
+                       self.c, n, num, num, num, num, quote)
+            },
             None => write!(f, "{} is unknown", self.c)
         }
     }
@@ -20,14 +27,19 @@ impl fmt::Display for Describable {
 /// or a unicode code point name) and returns either None (if the
 /// character description is not understood), or Some(Describable)
 /// that holds the character.
-fn from_arg(spec: &str) -> Option<Describable> {
+fn from_arg(spec: &str) -> Vec<Describable> {
+    let mut res : Vec<Describable> = Vec::new();
+
     if spec.chars().count() == 1 {
-        spec.chars().next().map(|c| Describable{c: c})
+        spec.chars().next().map(|c| res.push(Describable{c: c}));
     } else {
-        unicode_names::character(spec).map(|c| Describable{c: c})
+        unicode_names::character(spec).map(|c| res.push(Describable{c: c}));
     }
-    // TODO: \u, 0x, 0-prefixed chars, decimals too
-    // TODO: Return multiple chars if there's ambiguity.
+    for base in vec![10, 16, 8, 2] {
+        let _ = u32::from_str_radix(spec, base.clone()).ok().
+            map(|num| char::from_u32(num).map(|c| res.push(Describable{c: c})));
+    }
+    res
 }
 
 fn main() {
@@ -42,12 +54,20 @@ fn main() {
 
 #[test]
 fn from_arg_translates_chars() {
-    assert_eq!('c', from_arg("c").unwrap().c);
-    assert_eq!(']', from_arg("]").unwrap().c);
+    assert_eq!('c', from_arg("c")[0].c);
+    assert_eq!(']', from_arg("]")[0].c);
 }
 
 #[test]
 fn from_arg_translates_descriptions() {
-    assert_eq!('c', from_arg("latin small letter c").unwrap().c);
-    assert_eq!(']', from_arg("right square bracket").unwrap().c);
+    assert_eq!('c', from_arg("latin small letter c")[0].c);
+    assert_eq!(']', from_arg("right square bracket")[0].c);
+}
+
+#[test]
+fn from_arg_translates_numbers() {
+    let received = from_arg("60");
+    assert_eq!('<', received[0].c);
+    assert_eq!('`', received[1].c);
+    assert_eq!('0', received[2].c);
 }
