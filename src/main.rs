927310a3a8553793ca3af6570d6fe6992f4c6839
Andreas Fuchs <asf@boinkor.net>
2016-08-06 19:57:20 -0700

    Extract arg interpreter into a module
    Also implement ^-escapes for control char arguments - this lets us run
    chars ^c and get the description for ETX.

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,10 @@ use std::char;
 
 use byteorder::{ByteOrder, BigEndian};
 
+mod human_names;
 mod ascii;
 
-struct Describable {
+pub struct Describable {
     c: char,
 }
 
@@ -203,74 +204,11 @@ impl fmt::Display for ByteRepresentation {
     }
 }
 
-/// Takes a stringly description of a character (the character itself,
-/// or a unicode code point name) and returns a Vec of Describable
-/// elements that hold the corresponding character. The elements of
-/// the vector are sorted by descending numeric code point.
-fn from_arg(spec: &str) -> Vec<Describable> {
-    let mut chars: Vec<char> = Vec::new();
-
-    // match the character itself, or any of its names:
-    if spec.chars().count() == 1 {
-        spec.chars().next().map(|c| chars.push(c));
-    } else {
-        unicode_names::character(spec).map(|c| chars.push(c));
-    }
-    // Match hex/U+ strings specifically:
-    if spec.starts_with("0x") || spec.starts_with("U+") {
-        let _ = u32::from_str_radix(&spec[2..], 16).ok().
-            map(|num| char::from_u32(num).map(|c| chars.push(c)));
-    }
-
-    // Match plain numbers in all bases:
-    for base in vec![16, 10, 8, 2] {
-        let _ = u32::from_str_radix(spec, base.clone()).ok().
-            map(|num| char::from_u32(num).map(|c| chars.push(c)));
-    }
-
-    // Match characters by ascii(1) name / alias:
-    if let Some(ch) = ascii::lookup_by_name(spec) {
-        chars.push(ch);
-    }
-
-    chars.sort_by(|a, b| b.cmp(a));
-    chars.dedup();
-    chars.iter().map(|c| c.clone().into()).collect()
-}
-
 fn main() {
     let args =
         env::args().skip(1)
-        .flat_map(|argument| from_arg(argument.as_ref()));
+        .flat_map(|argument| human_names::from_arg(argument.as_ref()));
     for c in args {
         println!("{}", c);
     }
-}
-
-#[test]
-fn from_arg_translates_chars() {
-    assert_eq!('n', from_arg("n")[0].c);
-    assert_eq!(']', from_arg("]")[0].c);
-}
-
-#[test]
-fn from_arg_translates_descriptions() {
-    assert_eq!('n', from_arg("latin small letter n")[0].c);
-    assert_eq!(']', from_arg("right square bracket")[0].c);
-}
-
-#[test]
-fn from_arg_translates_numbers() {
-    let received = from_arg("60");
-    let mut iter = received.iter();
-    assert_eq!('`', iter.next().unwrap().c);
-    assert_eq!('<', iter.next().unwrap().c);
-    assert_eq!('0', iter.next().unwrap().c);
-
-    assert_eq!(2, from_arg("0").len());
-    assert_eq!(0x30, from_arg("0").iter().next().unwrap().c as u32);
-
-    assert_eq!(1, from_arg("0x0").len());
-    assert_eq!(1, from_arg("0x41").len());
-    assert_eq!('A', from_arg("0x41")[0].c);
 }
